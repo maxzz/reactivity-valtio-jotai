527367e9e45bede9b1ff01b7b0980bc69025b9c1
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-27 14:09:24 -0700

    Initial setup. add alias resolving

--- src/components/form-vj/index.tsx
+++ src/components/form-vj/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+function Caption() {
+    return (
+        <div className="text-xl">
+            <div className="">Caption</div>
+            <div className=""></div>
+        </div>
+    );
+}
+
+function Body() {
+    return (
+        <div className="">Body</div>
+    );
+}
+
 export function FormValtioJotai() {
     return (
-        <div className="">
-            index 
+        <div className="h-full grid place-items-center">
+            <div className="w-[30rem] min-h-[40rem] border-slate-600 border rounded">
+                <Caption />
+                <Body />
+                index
             </div>
+        </div>
     );
 }
---
+++ src/components/ui/icons/index.tsx
@@ -0,0 +1,56 @@
+import { HTMLAttributes } from 'react';
+import { classNames } from '@/utils';
+
+export function IconStar({ title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path d="M 0 12 c 12 0 12 0 12 -12 c 0 12 0 12 12 12 c -12 0 -12 0 -12 12 c 0 -12 0 -12 -12 -12" />
+        </svg>
+    );
+}
+
+export function IconClose({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    );
+}
+
+export function IconMenu({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+    );
+}
+
+export function IconTrash({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+        </svg>
+    );
+}
+
+export function IconArrowUp({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+        </svg>
+    );
+}
+
+export function IconArrowDown({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+        </svg>
+    );
+}
