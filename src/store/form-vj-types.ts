42c32ac6eb19511e974c85c6fb0e0aa29cd6cb98
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-27 16:58:04 -0700

    WIP. store. add form-vj-initial-data.ts

--- src/store/form-vj-types.ts
+++ src/store/form-vj-types.ts
@@ -1,42 +1,56 @@
-export type FieldInForm = {
-    type: string;
-    disp: string;
-    value: string;
-};
+import { buildCatalogMetaFromNames, catalogTestNames } from "./form-vj-initial-data";
+
+export declare module Catalog { // pmat/include/ots_storagecatalog_io.h
+    interface Descriptor {
+        id?: string;            // default as guid
+    }
+
+    interface Name {
+        dispname: string;
+        dbname: string;
+        value?: string;
+        ownernote?: string;
+        askalways?: boolean;    // undefined : '1' 
+        onetvalue?: boolean;    // undefined : '1'
+        password?: boolean;     // undefined : '1'
+    }
+
+    interface Root {
+        descriptor?: Descriptor;
+        names: Name[];
+    }
+
+} //module Catalog
+
+export type CatalogItem =       // Item in memory w/ meta information
+    Catalog.Name
+    & {
+        index: number;          // index in loaded file.
+        uuid: number;           // local (in memory only) unique ID (not updated through one session).
+        mru: number;            // most recently used timestamp (as uuid but updated on each use through one session)
+    };
+
+//
 
 export type FormVjInputs = {
     name: string,
-    firstName: string,
-    lastName: string,
-    radioIn: string;
-    title: string;
-    moreDetails: boolean;
-    details: string;
-    fields: FieldInForm[];
+    items: CatalogItem[];
 };
 
 export const formVjDefaultValues: FormVjInputs = {
     name: 'Max Z.',
-    firstName: 'Max',
-    lastName: '',
-    radioIn: '3',
-    title: 'Dr',
-    moreDetails: false,
-    details: 'Some additional details',
-    fields: [],
+    items: buildCatalogMetaFromNames(catalogTestNames),
 };
 
-//
+// field value choice
 
 export type SelectOption = {
     label: React.ReactNode;
     value: string | number | string[];
 };
 
-export const selectOptions: SelectOption[] = [
-    { label: 'Mr', value: 'Mr' },
-    { label: 'Mrs', value: 'Mrs' },
-    { label: 'Miss', value: 'Miss' },
-    { label: 'Dr', value: 'Dr' },
+export const LIST_valueAskNames: SelectOption[] = [
+    { label: "Ask - Resuse", value: 1, },
+    { label: "Ask - Confirm", value: 2, },
+    { label: "Ask Always ", value: 3, },
 ];
-
