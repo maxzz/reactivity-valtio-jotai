c617f06f11d5f9811cfa8356e56d5c761979caf9
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-27 15:14:43 -0700

    WIP. update initial screen background animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { FormValtioJotai } from "./components/form-vj";
 
 export function App() {
     return (<>
-        <div className="h-screen bg-slate-800 text-slate-400 text-sm">
+        <div className="fixed inset-0 -z-10 bg-red-500/20 pointer-events-none"></div>
+
+        {/* <div className="h-screen bg-slate-800 text-slate-400 text-sm">
             <FormValtioJotai />
-        </div>
+        </div> */}
     </>);
 }
