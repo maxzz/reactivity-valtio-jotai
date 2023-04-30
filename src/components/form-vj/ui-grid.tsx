4482bf52c27d855f28215beaf9c17f7c7f70b1c3
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-30 15:01:57 -0700

    WIP. ui-grid. ---------- remove atomWithProxy version w/ RowItemWithAtom

--- src/components/form-vj/ui-grid.tsx
+++ src/components/form-vj/ui-grid.tsx
@@ -4,40 +4,17 @@ import { CatalogItem } from "@/store/form-vj-types";
 import { MenuButtons, MenuState, openButtonClasses } from "./ui-grid-row-menu";
 import { IconMenu } from "../ui/icons";
 import { useClickAway } from "react-use";
-import { removeImm, swap, turnOffAutoComplete } from "@/utils";
-import { atomWithProxy } from "jotai-valtio";
-import { useAtom } from "jotai";
+import { swap, turnOffAutoComplete } from "@/utils";
 
 type StringRowKey = keyof Pick<CatalogItem, 'dispname' | 'dbname'>;
 
 function RowItem({ item, name = 'dispname', ...rest }: { item: CatalogItem; name: StringRowKey; } & InputHTMLAttributes<HTMLInputElement>) {
     const snap = useSnapshot(item, { sync: true });
-    const snapAtom = useState(atomWithProxy(item))[0];
-    // const snap = useState(atomWithProxy(item))[0];
-
-    console.log('row', { 'item': item, 'snap': snap });
-
     return (
         <input
             className="px-2 py-1 w-full text-xs text-primary-300 bg-primary-700 rounded" {...turnOffAutoComplete} {...rest}
             value={snap[name]}
             onChange={(e) => { item[name] = e.target.value; }}
-        />
-    );
-}
-
-function RowItemWithAtm({ item, name = 'dispname', ...rest }: { item: CatalogItem; name: StringRowKey; } & InputHTMLAttributes<HTMLInputElement>) {
-    const snap = useSnapshot(item, { sync: true });
-    const snapAtom = useState(atomWithProxy(item))[0];
-    const [snapAtomAccess, setSnapAtomAccess] = useAtom(snapAtom);
-
-    console.log('row', { 'item': item, 'snap': snap });
-
-    return (
-        <input
-            className="px-2 py-1 w-full text-xs text-primary-300 bg-primary-700 rounded" {...turnOffAutoComplete} {...rest}
-            value={snapAtomAccess.dispname}
-            onChange={(e) => { setSnapAtomAccess((v) => ({ ...v, [name]: e.target.value })); }}
         />
     );
 }
@@ -50,25 +27,20 @@ type RowParams = {
 
 export function Row({ item, idx, menuState }: RowParams) {
     const [menuOpen, setMenuOpen] = useState(false);
-    const onClose = (event: React.MouseEvent) => { event.preventDefault(); setMenuOpen(v => !v); };
+    const onClose = () => setMenuOpen(v => !v);
     const btnRef = useRef(null);
-    useClickAway(btnRef, () => { setMenuOpen(false); });
+    useClickAway(btnRef, () => setMenuOpen(false));
 
-    console.log('item', item);
-    //const snap = useSnapshot(item);
+    const { password: isPsw = false } = useSnapshot(item);
     return (
         <div className="grid grid-cols-[1fr_1fr_1fr_auto] gap-x-1">
 
-            {/* <div className="">type {snap.password?'psw':'txt'}</div> */}
-            <RowItemWithAtm item={item} name="dispname" />
+            <div className="">type {isPsw ? 'psw' : 'txt'}</div>
             <RowItem item={item} name="dispname" />
             <RowItem item={item} name="dbname" />
 
             <button ref={btnRef} className="relative">
-                <IconMenu
-                    className={openButtonClasses}
-                    onClick={(event) => { event.preventDefault(); setMenuOpen(v => !v); }}
-                />
+                <IconMenu className={openButtonClasses} onClick={() => { setMenuOpen(v => !v); }} />
 
                 {menuOpen &&
                     <MenuButtons onClose={onClose} {...menuState} />
@@ -86,10 +58,9 @@ export function ItemsArray() {
         <div className="text-xs grid gap-y-1">
             {snap.items.map((item, idx) => {
                 const menuState: MenuState = {
-                    // onDelete: (event: React.MouseEvent) => { event.preventDefault(); form.items = removeImm(items, idx); },
-                    onDelete: (event: React.MouseEvent) => { event.preventDefault(); form.items.splice(idx, 1); },
-                    onUp: (event: React.MouseEvent) => { event.preventDefault(); idx > 0 && swap(items, idx - 1, idx); },
-                    onDn: (event: React.MouseEvent) => { event.preventDefault(); idx < items.length - 1 && swap(items, idx, idx + 1); },
+                    onDelete: () => { form.items.splice(idx, 1); },
+                    onUp: () => { idx > 0 && swap(items, idx - 1, idx); },
+                    onDn: () => { idx < items.length - 1 && swap(items, idx, idx + 1); },
                     hasUp: idx > 0,
                     hasDn: idx < snap.items.length - 1,
                 };
