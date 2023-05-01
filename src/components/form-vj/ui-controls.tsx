c31a8963fcadd33d3f4926bbc376b60abaf1b15a
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-30 18:55:06 -0700

    WIP. ui-grid. update buttom bottons to generic

--- src/components/form-vj/ui-controls.tsx
+++ src/components/form-vj/ui-controls.tsx
@@ -4,7 +4,7 @@ import { IconClose } from "../ui/icons";
 
 export const inputClasses = "px-2 py-1.5 w-full text-primary-300 bg-primary-700 rounded";
 export const inputFocusClasses = "focus:outline-none focus:ring-1 focus:ring-primary-400 focus:ring-offset-1 focus:ring-offset-primary-800";
-export const dlgBottomButtonClasses = "px-4 py-2 hover:bg-primary-700 border-primary-500 active:scale-[.97] border rounded select-none disabled:opacity-25";
+export const dlgBottomButtonClasses = "hover:bg-primary-700 border-primary-500 active:scale-[.97] border rounded select-none disabled:opacity-25";
 
 export function Caption() {
     return (
@@ -27,7 +27,7 @@ export function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputEleme
 
 export function BottomButton({ className, children, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <button className={classNames(dlgBottomButtonClasses, inputFocusClasses, className)} {...rest}>
+        <button className={classNames("px-4 py-2", dlgBottomButtonClasses, inputFocusClasses, className)} {...rest}>
             {children}
         </button>
     );
