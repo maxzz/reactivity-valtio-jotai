72670797386e8ce73b52c6ee8533e603ff510bcd
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-02 18:45:18 -0700

    WIP. footer. update layout

--- src/components/section3-footer/index.tsx
+++ src/components/section3-footer/index.tsx
@@ -5,15 +5,15 @@ import { IconSunnyvale } from "../ui/icons";
 function ColorModeSwitch() {
     const { darkMode } = useSnapshot(appUi);
     return (
-        <DarkLightSwitch isDark={darkMode} onClick={() => { appUi.darkMode = !darkMode; }} />
+        <DarkLightSwitch className="absolute right-3 w-4 h-4 cursor-pointer" isDark={darkMode} onClick={() => { appUi.darkMode = !darkMode; }} />
     );
 }
 
 export function Section3_Footer() {
     return (
-        <div className="px-2 py-2 text-primary-500 bg-primary-900 flex items-center justify-center">
-            <div className="">footer</div>
-            <IconSunnyvale className="w-5 h-5 text-primary-600" />
+        <div className="px-2 py-2 text-primary-500 bg-primary-900 select-none flex items-center justify-center relative">
+            <div className="text-xs">footer</div>
+            <IconSunnyvale className="w-5 h-5 pt-1 text-primary-600" />
 
             <ColorModeSwitch />
         </div>
