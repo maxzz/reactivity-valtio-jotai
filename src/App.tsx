e29ef404fc8ed8c96f539b0aab0ea670bebc4ba5
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-02 17:48:48 -0700

    WIP. app. add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Section2_Main } from "./components/section2-main";
 
+function Footer() {
+    return (
+        <div className="px-2 py-4 bg-primary-900">footer</div>
+    );
+}
+
 export function App() {
     return (<>
         <div className="appbg fixed inset-0 -z-10 pointer-events-none"></div>
 
-        <div className="h-screen bg-slate-800 text-slate-400 text-sm">
+        <div className="h-screen bg-slate-800 text-slate-400 text-sm grid grid-rows-[1fr,auto]">
             <Section2_Main />
+            <Footer />
         </div>
     </>);
 }
--- src/components/section2-main/index.tsx
+++ src/components/section2-main/index.tsx
@@ -1,10 +1,15 @@
+import { appUi, useSnapshot } from '@/store';
 import { FormValtioJotai } from '../form-vj';
 import { Display } from './ui-display';
 
 export function Section2_Main() {
+    const { pageVjDlgOpen } = useSnapshot(appUi);
     return (
-        <div className="mx-auto p-4 max-w-7xl h-full grid grid-cols-2 gap-x-4">
-            <FormValtioJotai />
+        <div className="mx-auto p-4 max-w-7xl h-full grid grid-cols-2 gap-x-4 overflow-auto">
+            {pageVjDlgOpen
+                ? <FormValtioJotai />
+                : <div></div>
+            }
 
             <div className="border-slate-600 border rounded overflow-hidden">
                 <Display />
