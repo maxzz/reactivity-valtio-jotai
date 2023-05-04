83838ab65e57d9e9a370bdd3d1bd0afd911ed601
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-04 15:17:12 -0700

    WIP. app. add Scroller2 component

--- src/components/section2-main/ui-display.tsx
+++ src/components/section2-main/ui-display.tsx
@@ -5,13 +5,44 @@ import * as ScrollArea from "@radix-ui/react-scroll-area";
 
 function Scroller2({ children }: { children: ReactNode; }) {
     return (
-        <ScrollArea.Root>
-            <ScrollArea.Viewport>
+        <ScrollArea.Root className=" h-full rounded overflow-hidden">
+            <ScrollArea.Viewport className="w-full h-full">
                 {children}
             </ScrollArea.Viewport>
 
-            <ScrollArea.Scrollbar orientation="vertical">
-                <ScrollArea.Thumb />
+            <ScrollArea.Scrollbar
+                orientation="vertical"
+                className="
+                flex 
+                select-none 
+                touch-none 
+                p-0.5 
+                bg-green-900
+                transition-colors 
+                duration-[160ms] 
+                ease-out 
+                hover:bg-blackA8 
+                data-[orientation=vertical]:w-2.5 
+                data-[orientation=horizontal]:flex-col 
+                data-[orientation=horizontal]:h-2.5
+                "
+            >
+                <ScrollArea.Thumb className="
+                flex-1 
+                bg-red-500 
+                rounded-[10px] 
+                relative before:content-[''] 
+                before:absolute 
+                before:top-1/2 
+                before:left-1/2 
+                before:-translate-x-1/2 
+                before:-translate-y-1/2 
+                before:w-full 
+                before:h-full 
+                before:min-w-[44px] 
+                before:min-h-[44px]
+                "
+                />
             </ScrollArea.Scrollbar>
 
             <ScrollArea.Corner />
