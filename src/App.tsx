e7ebb9e88c957920939385392c619e6e1e5d88d6
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-06 19:38:45 -0700

    WIP. app. fix dark mode for app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     return (<>
         <div className="appbg fixed inset-0 -z-10 pointer-events-none"></div>
 
-        <div className="h-screen bg-slate-800 text-slate-400 text-sm grid grid-rows-[1fr,auto]">
+        <div className="h-screen text-slate-800 bg-slate-200 dark:text-slate-400 dark:bg-slate-800 text-sm grid grid-rows-[1fr,auto]">
             <Section2_Main />
             <Section3_Footer />
         </div>
