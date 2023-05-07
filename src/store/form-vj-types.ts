c67f03d70e8f4921b10a7d86eca26004f7900292
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-07 14:28:17 -0700

    WIP. store. add new newItem flag to CatalogItem type

--- src/store/form-vj-types.ts
+++ src/store/form-vj-types.ts
@@ -1,36 +1,6 @@
 import { buildCatalogMetaFromNames, catalogTestNames } from "./form-vj-initial-data";
-
-export declare module Catalog { // pmat/include/ots_storagecatalog_io.h
-    interface Descriptor {
-        id?: string;            // default as guid
-    }
-
-    interface Name {
-        dispname: string;
-        dbname: string;
-        value?: string;
-        ownernote?: string;
-        askalways?: boolean;    // undefined : '1' 
-        onetvalue?: boolean;    // undefined : '1'
-        password?: boolean;     // undefined : '1'
-    }
-
-    interface Root {
-        descriptor?: Descriptor;
-        names: Name[];
-    }
-
-} //module Catalog
-
-export type CatalogItem =       // Item in memory w/ meta information
-    Catalog.Name
-    & {
-        index: number;          // index in loaded file.
-        uuid: number;           // local (in memory only) unique ID (not updated through one session).
-        mru: number;            // most recently used timestamp (as uuid but updated on each use through one session)
-    };
-
-//
+import { CatalogItem } from "./manifest/mani-types";
+export * from "./manifest/mani-types";;
 
 export type FormVjInputs = {
     name: string,
