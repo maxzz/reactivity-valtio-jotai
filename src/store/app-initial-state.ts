ddb2dfc7028bd3e204c93873659f0e4db2734b53
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-11 14:24:28 -0700

    WIP. store. review the storage load process

--- src/store/app-initial-state.ts
+++ src/store/app-initial-state.ts
@@ -1,3 +1,9 @@
+import { UiState } from ".";
+
 export function setInitialDarkMode(initialValue: boolean) {
     document.getElementsByTagName('body')[0].classList[initialValue ? 'add': 'remove']('dark');
+}
+
+export function setUiInitialState(initialUiState: UiState) {
+    setInitialDarkMode(initialUiState.darkMode);
 }
---
+++ src/store/app-storage.ts
@@ -0,0 +1,75 @@
+import { proxy, subscribe } from 'valtio';
+import { setUiInitialState } from '.';
+import { FormVjInputs, formVjDefaultValues } from './form-vj-types';
+import { mergeDefaultAndLoaded } from '@/utils';
+
+const STORAGE_UI_KEY = 'reactivity-valtio-jotai:ui';
+const STORAGE_DATA_KEY = 'reactivity-valtio-jotai:data';
+const STORAGE_UI_VER = 'v1';
+const STORAGE_DATA_VER = 'v1';
+
+export const enum ActivePage {
+    none,
+    pageVj,
+}
+
+export type UiState = {
+    darkMode: boolean;
+    activePage: ActivePage;
+    pageVjDlgOpen: boolean;
+};
+
+type AppUi = {
+    uiState: UiState,
+    formVjInputs: FormVjInputs;
+};
+
+const initialAppUi: AppUi = {
+    uiState: {
+        darkMode: false,
+        activePage: ActivePage.pageVj,
+        pageVjDlgOpen: true,
+    },
+
+    formVjInputs: formVjDefaultValues,
+};
+
+export const appUi = proxy<AppUi>(loadUiInitialState());
+
+setUiInitialState(appUi.uiState);
+
+// Local storage
+
+function loadUiInitialState(): AppUi {
+    const storeState = {} as AppUi;
+
+    const storageUi = localStorage.getItem(STORAGE_UI_KEY);
+    if (storageUi) {
+        try {
+            storeState.uiState = JSON.parse(storageUi)?.[STORAGE_UI_VER];
+        } catch (error) {
+        }
+    }
+
+    const storageData = localStorage.getItem(STORAGE_DATA_KEY);
+    if (storageData) {
+        try {
+            storeState.formVjInputs = JSON.parse(storageData)?.[STORAGE_DATA_VER];
+        } catch (error) {
+        }
+    }
+
+    return mergeDefaultAndLoaded(storeState, initialAppUi);
+}
+
+subscribe(appUi.uiState, () => {
+    //console.log('store ui  ', appUi.uiState);
+
+    localStorage.setItem(STORAGE_UI_KEY, JSON.stringify({ [STORAGE_UI_VER]: appUi.uiState }));
+});
+
+subscribe(appUi.formVjInputs, () => {
+    //console.log('store data', appUi.formVjInputs);
+
+    localStorage.setItem(STORAGE_DATA_KEY, JSON.stringify({ [STORAGE_DATA_VER]: appUi.formVjInputs }));
+});
