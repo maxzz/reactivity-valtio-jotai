a7cc12ce95198644cdd8eb9e9f48418517f53395
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-12 13:45:14 -0700

    WIP. FormValtioJotai. fix ButtonAdd layout

--- src/components/form-vj/ui-control-grid/ui-grid-add-button.tsx
+++ src/components/form-vj/ui-control-grid/ui-grid-add-button.tsx
@@ -2,11 +2,11 @@ import { useState, useRef, ButtonHTMLAttributes } from "react";
 import { addCatalogItem } from "@/store";
 import { useClickAway } from "react-use";
 import { classNames } from "@/utils";
-import { IconAdd } from "@/components/ui";
+import { IconAdd, IconFieldPassword, IconFieldText } from "@/components/ui";
 import { dlgBottomButtonClasses, inputFocusClasses } from "../dlg-controls";
 
-const menuBoxClasses = "absolute right-0 top-full mt-0.5 px-1 py-1.5 bg-primary-200 dark:bg-primary-900 border-gray-500/50 border shadow rounded animate-row-menu-left";
-const menuBtnClasses = "px-2 py-1 text-start hover:text-primary-800 dark:hover:text-primary-200 hover:bg-primary-400/50 dark:hover:bg-primary-500 whitespace-nowrap rounded-sm cursor-pointer";
+const menuBoxClasses = "absolute right-0 top-full mt-0.5 px-1 py-1.5 bg-primary-200 dark:bg-primary-900 border-gray-500/50 border rounded shadow animate-row-menu-left";
+const menuBtnClasses = "pl-1 pr-3 py-1 text-start hover:text-primary-800 dark:hover:text-primary-200 hover:bg-primary-400/50 dark:hover:bg-primary-500 flex items-center space-x-1 whitespace-nowrap rounded-sm cursor-pointer";
 
 export function ButtonAdd({ className, ...rest }: ButtonHTMLAttributes<HTMLElement>) {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -22,8 +22,14 @@ export function ButtonAdd({ className, ...rest }: ButtonHTMLAttributes<HTMLEleme
 
             {menuOpen &&
                 <div className={menuBoxClasses}>
-                    <div className={menuBtnClasses} onClick={() => onAdd(false)}>Text field</div>
-                    <div className={menuBtnClasses} onClick={() => onAdd(true)}>Password field</div>
+                    <div className={menuBtnClasses} onClick={() => onAdd(false)}>
+                        <IconFieldText className="w-6 h-6 pt-0.5"/>
+                        <span>Text field</span>
+                    </div>
+                    <div className={menuBtnClasses} onClick={() => onAdd(true)}>
+                        <IconFieldPassword className="w-6 h-6 pt-0.5"/>
+                        <span>Password field</span>
+                    </div>
                 </div>
             }
         </button>
