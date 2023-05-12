06a771e866f607e70f6ecd51368860d937cf72cd
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-11 17:21:58 -0700

    WIP. FormValtioJotai. add ButtonAddWOPopup

--- src/components/form-vj/ui-control-grid/ui-grid-add-button.tsx
+++ src/components/form-vj/ui-control-grid/ui-grid-add-button.tsx
@@ -5,8 +5,40 @@ import { classNames, uuid as uuidShort } from "@/utils";
 import { v4 } from "uuid";
 import { IconAdd } from "@/components/ui";
 import { dlgBottomButtonClasses, inputFocusClasses } from "../dlg-controls";
+import { AddPopupMenu } from "./ui-grid-add-menu";
 
 export function ButtonAdd({ className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
+    const items = appUi.formVjInputs.items;
+    const { length } = useSnapshot(items);
+    return (
+        <button
+            className={classNames(dlgBottomButtonClasses, inputFocusClasses, className)}
+            onClick={() => items.push(generateNewItem('name', length, false))}
+            {...rest}
+        >
+            <IconAdd />
+            <AddPopupMenu />
+        </button>
+    );
+
+    function generateNewItem(newName: string, newIndex: number, isPassword: boolean): CatalogItem {
+        const guid = v4();
+        const now = uuidShort.asRelativeNumber();
+        const rv: CatalogItem = {
+            dispname: 'name',
+            dbname: `{${guid}}`,
+            index: newIndex,
+            uuid: now,
+            mru: now,
+            newItem: true,
+        };
+        isPassword && (rv.password = true);
+        return rv;
+    }
+}
+
+/* 
+export function ButtonAddWOPopup({ className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
     const items = appUi.formVjInputs.items;
     const { length } = useSnapshot(items);
     return (
@@ -16,6 +48,7 @@ export function ButtonAdd({ className, ...rest }: ButtonHTMLAttributes<HTMLButto
             {...rest}
         >
             <IconAdd />
+            <AddPopupMenu />
         </button>
     );
 
@@ -32,3 +65,4 @@ export function ButtonAdd({ className, ...rest }: ButtonHTMLAttributes<HTMLButto
         };
     }
 }
+ */
