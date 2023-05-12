df39d119f2f52da6c5b08212ebe2ca17e8141c37
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-11 18:18:51 -0700

    WIP. FormValtioJotai. ButtonAdd2 code clean up. 3. simplify

--- src/components/form-vj/ui-control-grid/ui-grid-add-menu.tsx
+++ src/components/form-vj/ui-control-grid/ui-grid-add-menu.tsx
@@ -1,22 +1,12 @@
-import { useState, useRef, HTMLAttributes, ButtonHTMLAttributes } from "react";
+import { useState, useRef, ButtonHTMLAttributes } from "react";
+import { addCatalogItem } from "@/store";
 import { useClickAway } from "react-use";
 import { classNames } from "@/utils";
 import { IconAdd } from "@/components/ui";
-import { addCatalogItem } from "@/store";
 import { dlgBottomButtonClasses, inputFocusClasses } from "../dlg-controls";
-
-const openButtonClasses = "p-1 w-5 h-5 hover:text-primary-800 dark:hover:text-white hover:bg-primary-300 dark:hover:bg-primary-500 rounded";
 
 const menuBoxClasses = "absolute right-0 top-full mt-0.5 px-1 py-1.5 bg-primary-200 dark:bg-primary-900 border-gray-500/50 border shadow rounded animate-row-menu-left";
 const menuBtnClasses = "px-2 py-1 text-start hover:text-primary-800 dark:hover:text-primary-200 hover:bg-primary-400/50 dark:hover:bg-primary-500 whitespace-nowrap rounded-sm cursor-pointer";
-
-function Row({ children, ...rest }: HTMLAttributes<HTMLElement>) {
-    return (
-        <div className={menuBtnClasses} {...rest}>
-            {children}
-        </div>
-    );
-}
 
 export function ButtonAdd2({ className, ...rest }: ButtonHTMLAttributes<HTMLElement>) {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -35,8 +25,8 @@ export function ButtonAdd2({ className, ...rest }: ButtonHTMLAttributes<HTMLElem
 
             {menuOpen &&
                 <div className={menuBoxClasses}>
-                    <Row onClick={() => onAdd(false)}>Text field</Row>
-                    <Row onClick={() => onAdd(true)}>Password field</Row>
+                    <div className={menuBtnClasses} onClick={() => onAdd(false)}>Text field</div>
+                    <div className={menuBtnClasses} onClick={() => onAdd(true)}>Password field</div>
                 </div>
             }
         </button>
