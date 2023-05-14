d68f5d0c74749c2b6fbe201575566fd79423a929
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-13 19:25:48 -0700

    WIP. FormValtioJotai. add MruTrigger skeleton

--- src/components/form-vj/ui-contol-mru/index.tsx
+++ src/components/form-vj/ui-contol-mru/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
+import { IconMenu } from '@/components/ui';
+
+const openButtonClasses = "p-1 w-5 h-5 hover:text-primary-800 dark:hover:text-white hover:bg-primary-300 dark:hover:bg-primary-500 rounded";
 
 export function MruTrigger() {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <div>index</div>
+        <div>
+            <IconMenu className={openButtonClasses} onClick={() => setMenuOpen(v => !v)} />
+        </div>
     );
 }
