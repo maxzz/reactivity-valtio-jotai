d6db9cc4f66e3aa8f895f1edb6aeb88ad7c82253
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-13 19:35:05 -0700

    WIP. MruTrigger. fix popup layout look

--- src/components/form-vj/ui-contol-mru/index.tsx
+++ src/components/form-vj/ui-contol-mru/index.tsx
@@ -4,6 +4,13 @@ import { IconMenu } from '@/components/ui';
 
 const openButtonClasses = "w-5 h-5 hover:text-primary-800 dark:hover:text-white hover:bg-primary-300 dark:hover:bg-primary-500 rounded";
 
+function ItemsGrid() {
+    return (<>
+        <div className="">Item 1</div>
+        <div className="">Item 2</div>
+    </>);
+}
+
 export function MruTrigger() {
     const [menuOpen, setMenuOpen] = useState(false);
     const btnRef = useRef(null);
@@ -14,9 +21,10 @@ export function MruTrigger() {
             <IconMenu className={openButtonClasses} onClick={() => setMenuOpen(v => !v)} />
 
             {menuOpen &&
-                <div className="absolute left-0 top-full">11</div>
+                <div className="absolute left-0 top-full px-2 py-1 w-max min-w-[10ch] bg-primary-400 border-primary-400 border rounded">
+                    <ItemsGrid />
+                </div>
             }
-
         </div>
     );
 }
