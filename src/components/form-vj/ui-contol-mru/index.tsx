9eb783c69adce43c87c2c943f909e60dda6b1ff4
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-04 19:31:47 -0700

    WIP. update package dependencies. jotai

--- src/components/form-vj/ui-contol-mru/index.tsx
+++ src/components/form-vj/ui-contol-mru/index.tsx
@@ -3,6 +3,7 @@ import { useClickAway } from "react-use";
 import { IconMenu } from '@/components/ui';
 
 const openButtonClasses = "w-5 h-5 hover:text-primary-800 dark:hover:text-white hover:bg-primary-300 dark:hover:bg-primary-500 rounded";
+const popupClasses = "px-2 py-1 w-max min-w-[10ch] text-xs bg-primary-400 dark:bg-primary-700 border-primary-400 dark:border-primary-500 border rounded";
 
 function ItemsGrid() {
     return (<>
@@ -21,7 +22,7 @@ export function MruTrigger() {
             <IconMenu className={openButtonClasses} onClick={() => setMenuOpen(v => !v)} />
 
             {menuOpen &&
-                <div className="absolute left-0 top-full px-2 py-1 w-max min-w-[10ch] bg-primary-400 border-primary-400 border rounded">
+                <div className={`absolute left-0 top-full ${popupClasses}`}>
                     <ItemsGrid />
                 </div>
             }
