a5db10e3ad7bb2819597a1cc82c528961577b362
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-07 19:12:52 -0800

    WIP. layout. add MainForm animation

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "motion/react";
 import { appUi, useSnapshot } from '@/store';
 import { MainForm } from './1-form';
 import { Display } from './2-info-display';
@@ -8,9 +9,18 @@ export function Section2_Main() {
         // <div className="mx-auto p-4 max-w-7xl w-full h-full grid grid-cols-2 gap-x-4 overflow-auto">
         <div className="mx-auto p-4 max-w-7xl w-full h-full grid grid-cols-[minmax(1fr,600px),minmax(0,1fr)] gap-x-4 overflow-auto">
 
-            {pageVjDlgOpen && (
-                <MainForm />
-            )}
+            <AnimatePresence>
+                {pageVjDlgOpen && (
+                    <motion.div
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 10 }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        <MainForm />
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
             <div className="col-start-2 border-slate-600 border rounded overflow-hidden">
                 <Display />
