c8c3235f093c28337ceb5323248f29bae9157ccb
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-07 19:07:26 -0800

    WIP. layout. update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Section2_Main } from "./components/2-main";
 import { Section3_Footer } from "./components/3-footer";
 
+const mainClasses = "\
+h-screen \
+text-sm \
+text-slate-800 bg-slate-200 dark:text-slate-400 dark:bg-slate-800 \
+grid grid-rows-[1fr,auto]";
+
 export function App() {
     return (<>
         <div className="app-bg fixed inset-0 -z-10 pointer-events-none"></div>
 
-        <div className="h-screen text-slate-800 bg-slate-200 dark:text-slate-400 dark:bg-slate-800 text-sm grid grid-rows-[1fr,auto]">
+        <div className={mainClasses}>
             <Section2_Main />
             <Section3_Footer />
         </div>
