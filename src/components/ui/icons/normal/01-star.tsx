be7d17801c140a963940db1b1e33b3a59a214e29
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-08 18:52:54 -0800

    WIP. layout. update icons. normal. 1. add star, close

---
+++ src/components/ui/icons/normal/01-star.tsx
@@ -0,0 +1,11 @@
+import { type SVGAttributes } from 'react';
+import { classNames } from '@/utils';
+
+export function IconStar({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
+    return (
+        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path d="M 0 12 c 12 0 12 0 12 -12 c 0 12 0 12 12 12 c -12 0 -12 0 -12 12 c 0 -12 0 -12 -12 -12" />
+        </svg>
+    );
+}
--- src/components/ui/icons/normal/index.tsx
+++ src/components/ui/icons/normal/index.tsx
@@ -1,14 +1,7 @@
 import { type HTMLAttributes, type SVGAttributes } from 'react';
 import { classNames } from '@/utils';
 
-export function IconStar({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path d="M 0 12 c 12 0 12 0 12 -12 c 0 12 0 12 12 12 c -12 0 -12 0 -12 12 c 0 -12 0 -12 -12 -12" />
-        </svg>
-    );
-}
+export * from './01-star';
 
 export function IconClose({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
     return (
@@ -76,9 +69,9 @@ export function IconHamburger({ className, title, ...rest }: HTMLAttributes<SVGS
 {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
     <circle cx="12.1" cy="12.1" r="1" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
     </svg> */}
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   
-</svg>
+</svg> */}
 
 export function IconDot({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
     return (
