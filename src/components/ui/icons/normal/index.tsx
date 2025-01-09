ce3c20888241233072903fee565f4988121197f0
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-08 18:59:31 -0800

    WIP. layout. update icons. normal. 2. add menu, trash, arrow up/down

--- src/components/ui/icons/normal/index.tsx
+++ src/components/ui/icons/normal/index.tsx
@@ -1,54 +1,14 @@
-import { type HTMLAttributes, type SVGAttributes } from 'react';
+import { type SVGAttributes } from 'react';
 import { classNames } from '@/utils';
 
 export * from './01-star';
+export * from './02-close';
+export * from './03-menu';
+export * from './04-trash';
+export * from './05-arrow-up';
+export * from './06-arrow-down';
 
-export function IconClose({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-    );
-}
-
-export function IconMenu({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-    );
-}
-
-export function IconTrash({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-        </svg>
-    );
-}
-
-export function IconArrowUp({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
-        </svg>
-    );
-}
-
-export function IconArrowDown({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
-            {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
-        </svg>
-    );
-}
-
-export function IconAdd({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconAdd({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -57,7 +17,7 @@ export function IconAdd({ className, title, ...rest }: HTMLAttributes<SVGSVGElem
     );
 }
 
-export function IconHamburger({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconHamburger({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -73,7 +33,7 @@ export function IconHamburger({ className, title, ...rest }: HTMLAttributes<SVGS
   
 </svg> */}
 
-export function IconDot({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconDot({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -82,7 +42,7 @@ export function IconDot({ className, title, ...rest }: HTMLAttributes<SVGSVGElem
     );
 }
 
-export function IconCheck({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconCheck({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -91,7 +51,7 @@ export function IconCheck({ className, title, ...rest }: HTMLAttributes<SVGSVGEl
     );
 }
 
-export function IconChevronUp({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconChevronUp({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -100,7 +60,7 @@ export function IconChevronUp({ className, title, ...rest }: HTMLAttributes<SVGS
     );
 }
 
-export function IconChevronDown({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconChevronDown({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -109,7 +69,7 @@ export function IconChevronDown({ className, title, ...rest }: HTMLAttributes<SV
     );
 }
 
-export function IconChevronRight({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+export function IconChevronRight({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg className={classNames("fill-none stroke-current stroke-[1.5]", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
@@ -118,7 +78,7 @@ export function IconChevronRight({ className, title, ...rest }: HTMLAttributes<S
     );
 }
 
-export function IconSunnyvale({ className, title, ...rest }: HTMLAttributes<SVGSVGElement> & SVGAttributes<SVGSVGElement>) {
+export function IconSunnyvale({ className, title, ...rest }: SVGAttributes<SVGSVGElement> & { title?: string; }) {
     return (
         <svg viewBox="0 0 24 24" className={classNames("fill-current", className)} {...rest}>
             {title && <title>{title}</title>}
